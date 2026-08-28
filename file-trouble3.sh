cec5226eba95074a1baa7402a9dbcb599b6a944b
Sergei <sergei@fujitsu.local>
2026-08-28 10:50:08 +0000

    Chapter 30: Troubleshooting and debugging scripts
    Added  scripts for debugging, tracing, PS4 customization, and set -x usage.

---
+++ file-trouble3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# trouble: сценарий для демонстрации распространенных видов ошибок
+
+number=1
+
+set -x # Включить трасировку
+if [ $number = 1 ]; then
+    echo "Number is equal to 1."
+else
+    echo " Number is not equal to 1."
+fi
+set +x # Выключить трассировку
---
+++ stubs.sh
@@ -0,0 +1,12 @@
+if [[ -d $dir_name ]]; then
+    if cd $dir_name; then
+        echo rm * # ТЕСТИРОВАНИЕ
+    else
+        echo "cannot cd to '$dir_name'" >&2
+        exit 1
+    fi
+else
+    echo "no such directory: '$dir_name'" >&2
+    exit 1
+fi
+exit # ТЕСТИРОВАНИЕ
